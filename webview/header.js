75942bd8258ebf5d3d9d9d27a2339550b7ff525d
Ashwani Goswami <ashwanigoswami79@gmail.com>
2023-07-26 14:01:15 +0530

    fix

--- webview/header.js
+++ webview/header.js
@@ -155,13 +155,17 @@ exports.updateVisibility = async (req, res) => {
   let connection;
   try {
     const { HID, Hvisible } = req.body;
-    const updateH = "UPDATE header SET  visibility=$1 WHERE h_id=$2";
+    const updateH = "UPDATE header SET visibility=$1 WHERE h_id=$2";
     connection = await pool.connect();
-    const check_visibility = "select count HID FROM header WHERE visibility = true"
-    const result = await connection.query(check_visibility)
-    if (result.rowCount>2) {
-      return res.status(400).send({ message: "Only two images are allowed to display in header" });
+
+    const check_visibility = "SELECT COUNT(*) AS visible_count FROM header WHERE visibility = true";
+    const result = await connection.query(check_visibility);
+    const visibleCount = parseInt(result.rows[0].visible_count);
+
+    if (Hvisible && visibleCount >= 2) {
+      return res.status(400).send({ message: "Only two images are allowed to display in the header" });
     }
+
     const updateHeader = await connection.query(updateH, [Hvisible, HID]);
     return res.status(200).send({ message: "Successfully Updated!" });
   } catch (error) {
@@ -173,6 +177,7 @@ exports.updateVisibility = async (req, res) => {
     }
   }
 };
+
 
 exports.deleteHeader=async(req,res)=>{
   let connection
