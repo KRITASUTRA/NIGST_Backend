accb57c96fdfff7251c3c5b92d01161b687d32bf
Ashwani Goswami <ashwanigoswami79@gmail.com>
2023-12-11 11:55:50 +0530

    ip test again

--- middleware/limiter.js
+++ middleware/limiter.js
@@ -8,7 +8,7 @@ const IPlimiter = rateLimit({
   legacyHeaders: false,
   message: 'Too many login attempts from this IP. Try again later.',
   keyGenerator: function (req,res) {
-    // console.log(req.headers["x-forwarded-for"])
+    console.log(req.ip)
     return req.headers["x-forwarded-for"] || req.connection.remoteAddress;
  
 }
